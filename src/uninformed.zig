50392ab324deaff25b2f7b766a62d84e0d32fa70
Rekihyt <checkmateguy@gmail.com>
2022-06-09 23:38:07 -0700

    start bfs impl

--- src/uninformed.zig
+++ src/uninformed.zig
@@ -4,61 +4,86 @@ const ArrayList = std.ArrayList;
 const print = std.debug.print;
 const panic = std.debug.panic;
 const TailQueue = std.TailQueue;
-const Node = @import("graph.zig").Node;
+const graph = @import("graph.zig");
+const Allocator = std.mem.Allocator;
+const AutoHashMap = std.AutoHashMap;
+const LinearFifo = std.fifo.LinearFifo;
 
-// Uses a `TailQueue` as a LIFO data structure.
-pub fn dfs(
+pub fn bfs(
     allocator: Allocator,
-    initial: anytype,
-    context: anytype,
-    goalTest: fn (context: @TypeOf(initial), node: @TypeOf(initial)) bool,
-) ?*@TypeOf(initial) {
-    // Initially, the frontier consists of just the start node.
-    const frontier = TailQueue(NodeType).init(allocator);
-    _ = goalTest;
+    initial: anytype, // a node pointer type
+    context: anytype, // anytype
+    goalTest: fn (context: @TypeOf(context), node: @TypeOf(initial)) bool,
+) ?@TypeOf(initial) {
+    const Node = @TypeOf(initial);
+    const frontier = LinearFifo(u8, .Dynamic).init(allocator);
+    defer frontier.deinit();
+
+    var explored = AutoHashMap(*Node, void).init(allocator);
+    defer explored.deinit();
+
     var node = initial;
-    while (!goalTest(context, node)) {
-        frontier.
+    while (!goalTest(context, frontier.read())) {
+        //for
+        try frontier.write(node.edges.keys());
     }
     return node;
 }
 
-const allocator = std.testing.allocator;
+// Uses a `TailQueue` as a FIFO data structure.
+// pub fn dfs(
+//     allocator: Allocator,
+//     initial: anytype, // a node pointer type
+//     context: anytype, // anytype
+//     goalTest: fn (context: @TypeOf(context), node: @TypeOf(initial)) bool,
+// ) ?@TypeOf(initial) {
+//     const Node = @TypeOf(initial);
+//     // Initially, the frontier consists of just the start node.
+//     const frontier = TailQueue(Node).init(allocator);
+//     defer frontier.deinit();
 
-test "dfs depth 0" {
-    const NodeType = Node([]const u8, u32);
+//     _ = goalTest;
+//     var node = initial;
+//     while (!goalTest(context, node)) {
+//         frontier.popFirst();
+//         // for
+//         try fifo.write("HELLO");
+//     }
+//     return node;
+// }
+
+test "bfs depth 0" {
+    const NodeType = graph.Node([]const u8, u32);
+    const allocator = std.testing.allocator;
     const node1 = try NodeType.init(allocator, "n1");
     defer node1.deinit(allocator);
 
-    if (dfs(
-        node1,
-        node1,
-        struct {
-            pub fn pred(initial, node: *NodeType) bool {
-                return node == initial;
-            }
-        }.pred,
-    )) |goal| {
+    if (bfs(allocator, node1, node1, struct {
+        pub fn pred(initial: *NodeType, node: *NodeType) bool {
+            return node == initial;
+        }
+    }.pred)) |goal| {
         if (goal != node1)
             // Shouldn't be possible
-            panic("Incorrect goal found");
+            panic("Incorrect goal found", .{});
     } else {
-        panic("Node not found");
+        panic("Node not found", .{});
     }
 }
 
 test "dfs depth 1" {
-    var node1 = try Node([]const u8, u32).init(allocator, "n1");
+    const allocator = std.testing.allocator;
+    var node1 = try graph.Node([]const u8, u32).init(allocator, "n1");
     defer node1.destroy();
-    var node2 = try Node([]const u8, u32).initWithEdges(allocator, "n2", &.{});
+    var node2 = try graph.Node([]const u8, u32).initWithEdges(allocator, "n2", &.{});
     defer node2.destroy();
-    var node3 = try Node([]const u8, u32).initWithEdges(allocator, "n3", &.{});
+    var node3 = try graph.Node([]const u8, u32).initWithEdges(allocator, "n3", &.{});
     defer node3.destroy();
-    const node4 = try Node([]const u8, u32).initWithEdges(
+    const node4 = try graph.Node([]const u8, u32).initWithEdges(
         allocator,
         "n4",
         // Segfaults if an anon struct is used instead
-        &[_]Node([]const u8, u32).Edge{
+        &[_]graph.Node([]const u8, u32).Edge{
             .{
                 .that = node1,
                 .weight = 41,
